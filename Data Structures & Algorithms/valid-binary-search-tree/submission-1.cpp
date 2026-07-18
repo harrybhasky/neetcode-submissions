f7bc0cdf24bd4213aa4f938df0449c5bbc76aa3c
Hari Bhaskaran <146609420+harrybhasky@users.noreply.github.com>
2026-07-18 16:50:04 +0530

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return validate(root, nullptr, nullptr);
+    }
+
+private:
+    bool validate(TreeNode* node, TreeNode* lower, TreeNode* upper) {
+        if (node == nullptr) {
+            return true;
+        }
+
+        if (lower != nullptr && node->val <= lower->val) {
+            return false;
+        }
+        if (upper != nullptr && node->val >= upper->val) {
+            return false;
+        }
+
+        return validate(node->left, lower, node) &&
+               validate(node->right, node, upper);
+    }
+};
